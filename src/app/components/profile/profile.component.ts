5fd2fe6ff4b15b34c61498880f80b12454102014
Juan D. Hernandez <juandanielhb@gmail.com>
2019-03-23 00:51:39 -0500

    Layout user profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -2,12 +2,39 @@ import { Component } from '@angular/core';
 
 
 @Component({
-    selector: 'Profile',
-    templateUrl: './Profile.component.html'    
+    selector: 'profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
     public title:string;
     public status:string;
+    public menuOptions = [
+        {
+            "buttonClass": "btn-success",
+            "iconClass": "fa-home",
+            "spanContent": "Inicio",
+            "routerLink": "/inicio/add"
+        },  
+        {
+            "buttonClass": "btn-danger",
+            "iconClass": "fa-newspaper",
+            "spanContent": "Noticias",
+            "routerLink": "/landing"
+        },  
+        {
+            "buttonClass": "btn-warning pl-3",
+            "iconClass": "fa-file-alt",
+            "spanContent": "Lecciones",
+            "routerLink": "/landing"
+        },                       
+        {
+            "buttonClass": "btn-info pl-3",
+            "iconClass": "fa-paperclip",
+            "spanContent": "Recursos",
+            "routerLink": "/landing"
+        }                      
+    ];
     
 
     constructor() { 
--- src/app/home/components/main/main.component.ts
+++ src/app/home/components/main/main.component.ts
@@ -6,13 +6,40 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-    public title:string;
-    
-    constructor() { 
+    public title: string;
+    public menuOptions = [
+        {
+            "buttonClass": "btn-success",
+            "iconClass": "fa-home",
+            "spanContent": "Inicio",
+            "routerLink": "/inicio/add"
+        },  
+        {
+            "buttonClass": "btn-danger",
+            "iconClass": "fa-newspaper",
+            "spanContent": "Noticias",
+            "routerLink": "/landing"
+        },  
+        {
+            "buttonClass": "btn-warning pl-3",
+            "iconClass": "fa-file-alt",
+            "spanContent": "Lecciones",
+            "routerLink": "/landing"
+        },                       
+        {
+            "buttonClass": "btn-info pl-3",
+            "iconClass": "fa-paperclip",
+            "spanContent": "Recursos",
+            "routerLink": "/landing"
+        }                      
+    ];
+
+    constructor() {
         this.title = 'Main';
+
     }
 
-    ngOnInit(): void { 
+    ngOnInit(): void {
         console.log("Inicializado " + this.title);
     }
 }
